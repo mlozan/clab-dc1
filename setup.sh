2a1543549b2ec1d2ed609e0070daa76c19e3a65c
mlozan <marialozanocasero@gmail.com>
2024-05-07 01:20:59 +0200

    correccion setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Lista de comandos a ejecutar para cada host
+declare -A comandos=(
+    ["h1"]="sudo ip addr add 192.168.1.11/24 dev eth1;
+           sudo ip route add 192.168.0.0/16 via 192.168.1.1 dev eth1;
+           sudo ip route add 172.16.0.0/16 via 192.168.1.1 dev eth1;"
+
+    ["h2"]="sudo ip addr add 172.16.1.12/24 dev eth1;
+           sudo ip route add 192.168.0.0/16 via 172.16.1.1 dev eth1;
+           sudo ip route add 172.16.0.0/16 via 172.16.1.1 dev eth1;"
+
+    ["h3"]="sudo ip addr add 192.168.3.2/30 dev eth1;
+           sudo ip route add 192.168.0.0/16 via 192.168.3.1 dev eth1;
+           sudo ip route add 172.16.0.0/16 via 192.168.3.1 dev eth1;"
+
+    ["h4"]="sudo ip addr add 172.16.3.2/24 dev eth1;
+           sudo ip route add 192.168.0.0/16 via 172.16.3.1 dev eth1;
+           sudo ip route add 172.16.0.0/16 via 172.16.3.1 dev eth1;"
+)
+
 # Carpeta base donde se encuentran las carpetas eBGP, iBGP e interfaces
 base_folder="configs"
 
@@ -14,6 +33,16 @@ nodos=("l1" "l2" "l3" "l4" "s1" "s2" "bl1" "bl2")
 
 # Lista de nombres de los hosts
 hosts=("h1" "h2" "h3" "h4")
+
+# Iterar sobre cada host y ejecutar los comandos correspondientes
+for host in "${!comandos[@]}"; do
+    contenedor="clab-my_dc1-$host"
+    
+    # Ejecutar los comandos para el host actual
+    for comando in "${comandos[$host]}"; do
+        docker exec -ti "$contenedor" bash -c "$comando"
+    done
+done
 
 # Iterar sobre cada carpeta y nodo
 for carpeta in "${carpetas[@]}"; do
@@ -37,5 +66,3 @@ for carpeta in "${carpetas[@]}"; do
         fi
     done
 done
-
-
