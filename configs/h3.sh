41a86d6a288af88dc0d4107e3a9058347ac93a8e
mlozan <marialozanocasero@gmail.com>
2024-05-08 12:37:20 +0200

    start-up config hosts 2

--- configs/h3.sh
+++ configs/h3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Comandos para el host h3
-sudo ip addr add 192.168.3.2/30 dev eth1
-sudo ip route add 192.168.0.0/16 via 192.168.3.1 dev eth1
-sudo ip route add 172.16.0.0/16 via 192.168.3.1 dev eth1
+docker exec -ti clab-my_dc1-h3 sudo ip addr add 192.168.3.2/30 dev eth1
+docker exec -ti clab-my_dc1-h3 sudo ip route add 192.168.0.0/16 via 192.168.3.1 dev eth1
+docker exec -ti clab-my_dc1-h3 sudo ip route add 172.16.0.0/16 via 192.168.3.1 dev eth1
