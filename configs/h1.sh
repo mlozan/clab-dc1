2082f886ab13379f0da67441eca9f55d102d6a25
mlozan <marialozanocasero@gmail.com>
2024-05-08 12:44:57 +0200

    start-up config hosts 3

--- configs/h1.sh
+++ configs/h1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Comandos para el host h1
-docker exec -ti clab-my_dc1-h1 sudo ip addr add 192.168.1.11/24 dev eth1
-docker exec -ti clab-my_dc1-h1 sudo ip route add 192.168.0.0/16 via 192.168.1.1 dev eth1
-docker exec -ti clab-my_dc1-h1 sudo ip route add 172.16.0.0/16 via 192.168.1.1 dev eth1
+sudo ip addr add 192.168.1.11/24 dev eth1
+sudo ip route add 192.168.0.0/16 via 192.168.1.1 dev eth1
+sudo ip route add 172.16.0.0/16 via 192.168.1.1 dev eth1
