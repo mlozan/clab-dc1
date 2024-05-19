e4b57e179b68f01ae05ea34285af2fe8fe46f84e
mlozan <marialozanocasero@gmail.com>
2024-05-19 17:10:16 +0200

    mtu = 8900

--- setup.sh
+++ setup.sh
@@ -2,18 +2,34 @@
 
 # Lista de comandos a ejecutar para cada host en my_dc1_ip.yaml
 declare -A comandos_ip=(
-    ["h1"]="sudo ip addr add 192.168.1.11/24 dev eth1; sudo ip route add 192.168.0.0/16 via 192.168.1.1 dev eth1; sudo ip route add 172.16.0.0/16 via 192.168.1.1 dev eth1"
-    ["h2"]="sudo ip addr add 172.16.1.12/24 dev eth1; sudo ip route add 192.168.0.0/16 via 172.16.1.1 dev eth1; sudo ip route add 172.16.0.0/16 via 172.16.1.1 dev eth1"
-    ["h3"]="sudo ip addr add 192.168.3.2/30 dev eth1; sudo ip route add 192.168.0.0/16 via 192.168.3.1 dev eth1; sudo ip route add 172.16.0.0/16 via 192.168.3.1 dev eth1"
-    ["h4"]="sudo ip addr add 172.16.3.2/24 dev eth1; sudo ip route add 192.168.0.0/16 via 172.16.3.1 dev eth1; sudo ip route add 172.16.0.0/16 via 172.16.3.1 dev eth1"
+    ["h1"]="sudo ip addr add 192.168.1.11/24 dev eth1; 
+            sudo ip route add 192.168.0.0/16 via 192.168.1.1 dev eth1; 
+            sudo ip route add 172.16.0.0/16 via 192.168.1.1 dev eth1;
+            sudo ifconfig eth1 mtu 8900;"
+    ["h2"]="sudo ip addr add 172.16.1.12/24 dev eth1; 
+            sudo ip route add 192.168.0.0/16 via 172.16.1.1 dev eth1; 
+            sudo ip route add 172.16.0.0/16 via 172.16.1.1 dev eth1;
+            sudo ifconfig eth1 mtu 8900;"
+    ["h3"]="sudo ip addr add 192.168.3.2/30 dev eth1; 
+            sudo ip route add 192.168.0.0/16 via 192.168.3.1 dev eth1; 
+            sudo ip route add 172.16.0.0/16 via 192.168.3.1 dev eth1;
+            sudo ifconfig eth1 mtu 8900;"
+    ["h4"]="sudo ip addr add 172.16.3.2/24 dev eth1; 
+            sudo ip route add 192.168.0.0/16 via 172.16.3.1 dev eth1; 
+            sudo ip route add 172.16.0.0/16 via 172.16.3.1 dev eth1;
+            sudo ifconfig eth1 mtu 8900;"
 )
 
 # Lista de comandos a ejecutar para cada host en my_dc1_mac.yaml
 declare -A comandos_mac=(
-    ["h1"]="sudo ip addr add 172.16.1.12/24 dev eth1"
-    ["h2"]="sudo ip addr add 172.16.1.14/24 dev eth1"
-    ["h3"]="sudo ip addr add 172.16.1.16/24 dev eth1"
-    ["h4"]="sudo ip addr add 172.16.1.18/24 dev eth1"
+    ["h1"]="sudo ip addr add 172.16.1.12/24 dev eth1;
+            sudo ifconfig eth1 mtu 8900;"
+    ["h2"]="sudo ip addr add 172.16.1.14/24 dev eth1;
+            sudo ifconfig eth1 mtu 8900;"
+    ["h3"]="sudo ip addr add 172.16.1.16/24 dev eth1;
+            sudo ifconfig eth1 mtu 8900;"
+    ["h4"]="sudo ip addr add 172.16.1.18/24 dev eth1;
+            sudo ifconfig eth1 mtu 8900;"
 )
 
 # Validar el argumento pasado al script
