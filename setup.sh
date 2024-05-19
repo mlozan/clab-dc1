fd3b44a7284766322d6964888b9f24090883e232
mlozan <marialozanocasero@gmail.com>
2024-05-19 14:49:39 +0200

    correccion setup 7

--- setup.sh
+++ setup.sh
@@ -4,19 +4,19 @@
 declare -A comandos_ip=(
     ["h1"]="sudo ip addr add 192.168.1.11/24 dev eth1;
            sudo ip route add 192.168.0.0/16 via 192.168.1.1 dev eth1;
-           sudo ip route add 172.16.0.0/16 via 192.168.1.1 dev eth1;"
+           sudo ip route add 172.16.0.0/16 via 192.168.1.1 dev eth1"
 
     ["h2"]="sudo ip addr add 172.16.1.12/24 dev eth1;
            sudo ip route add 192.168.0.0/16 via 172.16.1.1 dev eth1;
-           sudo ip route add 172.16.0.0/16 via 172.16.1.1 dev eth1;"
+           sudo ip route add 172.16.0.0/16 via 172.16.1.1 dev eth1"
 
     ["h3"]="sudo ip addr add 192.168.3.2/30 dev eth1;
            sudo ip route add 192.168.0.0/16 via 192.168.3.1 dev eth1;
-           sudo ip route add 172.16.0.0/16 via 192.168.3.1 dev eth1;"
+           sudo ip route add 172.16.0.0/16 via 192.168.3.1 dev eth1"
 
     ["h4"]="sudo ip addr add 172.16.3.2/24 dev eth1;
            sudo ip route add 192.168.0.0/16 via 172.16.3.1 dev eth1;
-           sudo ip route add 172.16.0.0/16 via 172.16.3.1 dev eth1;"
+           sudo ip route add 172.16.0.0/16 via 172.16.3.1 dev eth1"
 )
 
 # Lista de comandos a ejecutar para cada host en my_dc1_mac.yaml
